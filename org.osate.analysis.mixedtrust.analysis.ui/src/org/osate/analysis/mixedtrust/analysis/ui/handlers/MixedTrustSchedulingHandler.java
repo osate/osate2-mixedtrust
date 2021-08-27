2ad5771f9e9517fdabb05ea04d3e20148a77e08e
Aaron Greenhouse <aaron.greenhouse@gmail.com>
2021-08-27 13:42:51 -0400

    Created UI project

---
+++ org.osate.analysis.mixedtrust.analysis.ui/src/org/osate/analysis/mixedtrust/analysis/ui/handlers/MixedTrustSchedulingHandler.java
@@ -0,0 +1,15 @@
+package org.osate.analysis.mixedtrust.analysis.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class MixedTrustSchedulingHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		System.out.println("Hello!");
+
+		return null;
+	}
+}
