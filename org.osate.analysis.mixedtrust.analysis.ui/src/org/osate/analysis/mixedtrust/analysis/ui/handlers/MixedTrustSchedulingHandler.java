fbdfcd8db83e4e44d132662fb53bf4db6bb44479
Aaron Greenhouse <aaron.greenhouse@gmail.com>
2021-08-31 14:00:37 -0400

    Added UI COmmand handler for the mixed trust analysis.  Added warning/error messages to the analysis.  Still need to convert those messages to markers.

--- org.osate.analysis.mixedtrust.analysis.ui/src/org/osate/analysis/mixedtrust/analysis/ui/handlers/MixedTrustSchedulingHandler.java
+++ org.osate.analysis.mixedtrust.analysis.ui/src/org/osate/analysis/mixedtrust/analysis/ui/handlers/MixedTrustSchedulingHandler.java
@@ -1,15 +1,81 @@
 package org.osate.analysis.mixedtrust.analysis.ui.handlers;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.core.runtime.jobs.Job;
+import org.osate.aadl2.instance.SystemInstance;
+import org.osate.aadl2.modelsupport.errorreporting.AnalysisErrorReporterManager;
+import org.osate.aadl2.modelsupport.errorreporting.MarkerAnalysisErrorReporter;
+import org.osate.aadl2.modelsupport.util.AadlUtil;
+import org.osate.analysis.mixedtrust.analysis.MixedTrustAnalysis;
+import org.osate.result.AnalysisResult;
+import org.osate.ui.handlers.AbstractAnalysisHandler;
 
-public class MixedTrustSchedulingHandler extends AbstractHandler {
+public final class MixedTrustSchedulingHandler extends AbstractAnalysisHandler {
+	private static final String MARKER_TYPE = "org.osate.analysis.mixedtrust.analysis.ui.MixedTrustSchedulingAnalysisMarker";
+	private static final String OUTPUT_FILE_SUFFIX = "__MixedTrust.csv";
+	private static final String REPORT_SUB_DIR = "MixedTrust";
+
+	public MixedTrustSchedulingHandler() {
+		super();
+	}
 
 	@Override
-	public Object execute(final ExecutionEvent event) throws ExecutionException {
-		System.out.println("Hello!");
+	protected String getSubDirName() {
+		return REPORT_SUB_DIR;
+	}
 
-		return null;
+	@Override
+	protected String getOutputFileForAaxlFile(final IFile aaxlFile, final String filename) {
+		return filename + OUTPUT_FILE_SUFFIX;
+	}
+
+
+	@Override
+	protected Job createAnalysisJob(final IFile aaxlFile, final IFile outputFile) {
+		return new MixedTrustJob(aaxlFile, outputFile);
+	}
+
+	private final class MixedTrustJob extends WorkspaceJob {
+		private final IFile aaxlFile;
+		private final IFile outputFile;
+
+		public MixedTrustJob(final IFile aaxlFile, final IFile outputFile) {
+			super("Mixed trust scheduling analysis of " + aaxlFile.getName());
+			this.aaxlFile = aaxlFile;
+			this.outputFile = outputFile;
+		}
+
+		@Override
+		public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
+			final AnalysisErrorReporterManager errManager = new AnalysisErrorReporterManager(
+					new MarkerAnalysisErrorReporter.Factory(MARKER_TYPE));
+
+			// Three phases (1) analysis, (2) marker generation, (3) csv file
+			final SubMonitor subMonitor = SubMonitor.convert(monitor, 3);
+			boolean cancelled = false;
+
+			try {
+				final AnalysisResult analysisResult = new MixedTrustAnalysis().invoke(subMonitor.split(1),
+						(SystemInstance) AadlUtil.getElement(aaxlFile));
+				if (subMonitor.isCanceled()) {
+					throw new OperationCanceledException();
+				}
+//				generateMarkers(analysisResult, errManager);
+				subMonitor.worked(1);
+//				writeCSVFile(analysisResult, outputFile, subMonitor.split(1));
+			} catch (final OperationCanceledException e) {
+				cancelled = true;
+			}
+
+			return cancelled ? Status.CANCEL_STATUS : Status.OK_STATUS;
+		}
+
 	}
 }
--- org.osate.analysis.mixedtrust.analysis/src/org/osate/analysis/mixedtrust/analysis/MixedTrustAnalysis.java
+++ org.osate.analysis.mixedtrust.analysis/src/org/osate/analysis/mixedtrust/analysis/MixedTrustAnalysis.java
@@ -10,7 +10,9 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.osate.aadl2.ComponentCategory;
+import org.osate.aadl2.RecordValue;
 import org.osate.aadl2.contrib.deployment.DeploymentProperties;
 import org.osate.aadl2.contrib.timing.TimingProperties;
 import org.osate.aadl2.instance.ComponentInstance;
@@ -18,11 +20,63 @@ import org.osate.aadl2.instance.InstanceObject;
 import org.osate.aadl2.instance.SystemInstance;
 import org.osate.aadl2.instance.SystemOperationMode;
 import org.osate.aadl2.modelsupport.modeltraversal.SOMIterator;
+import org.osate.aadl2.util.Aadl2Util;
 import org.osate.analysis.mixedtrust.contribution.mixedtrustproperties.MixedTrustBindings;
+import org.osate.analysis.mixedtrust.contribution.mixedtrustproperties.MixedTrustProperties;
 import org.osate.analysis.mixedtrust.contribution.mixedtrustproperties.MixedTrustTask;
 import org.osate.result.AnalysisResult;
+import org.osate.result.DiagnosticType;
+import org.osate.result.Result;
+import org.osate.result.ResultType;
+import org.osate.result.util.ResultUtil;
 import org.osate.xtext.aadl2.properties.util.InstanceModelUtil;
 
+/**
+ * Class for performing mixed trust scheduling analysis on a system.
+ *
+ * <p>The format for the returned {@code AnalysisResult} object is described below.  In this case, the diagnostics
+ * are attached to the {@code Result} object for the system operation mode.  We do this because if the necessary property
+ * associations are inconsistent then we cannot build a sensible model for analysis, and there won't be any results
+ * below the SOM node.
+ *
+ * <p>For the {@code AnalysisResult} object itself:
+ * <ul>
+ *   <li>analysis = "Mixed Trust Scheduling"
+ *   <li>modelElement = {@code SystemInstance} being analyzed
+ *   <li>resultType = SUCCESS
+ *   <li>message = "Mixed Trust Scheduling of ..."
+ *   <li>diagnostics = empty list
+ *   <li>parameters = empty list
+ *   <li>results = one {@code Result} for each system operation mode
+ *     <ul>
+ *       <li>modelElement = {@code SystemOperationMode} instance object
+ *       <li>resultType = SUCCESS
+ *       <li>message = "" if the SOM is {@code null} or the empty SOM, otherwise "(xxx, ..., yyy)"
+ *       <li>values = empty list
+ *       <li>diagnostics = Diagnostics for the state of the system in the system operation mode.
+ *       <li>subResults = one {@code Result} for each {@code ComponentInstance} with category of {@code Processor} that
+ *       has a consistent property association for {@code Mixed_Trust_Processor}:
+ *         <ul>
+ *           <li>modelElement = {@code ComponentInstance} instance object
+ *           <li>resultType = SUCCESS
+ *           <li>message = The component's name from {@link ComponentInstance#getName()}
+ *           <li>values[0] = TRUE or FALSE indicating whether the processor's tasks are schedulable (BooleanValue)
+ *           <li>diagnostics = empty list
+ *           <li>subResults = one {@Result} for each thread bound to a hyper visor or guest os running on this processor
+ *             <ul>
+ *               <li>modelElement = {@code ComponentInstance} instance object for the thread
+ *               <li>resultType = SUCCESS
+ *               <li>message = The component's name from {@link ComponentInstance#getName()}
+ *               <li>values[0] = The priority value for the task (IntegerValue)
+ *               <li>diagnostics = empty list
+ *               <li>subResults = empty list
+ *             </ul>
+ *         </ul>
+ *     </ul>
+ * </ul>
+ *
+ * @since 4.0
+ */
 public final class MixedTrustAnalysis {
 	public MixedTrustAnalysis() {
 		super();
@@ -49,63 +103,99 @@ public final class MixedTrustAnalysis {
 
 	// TODO: Return AnalysisResult object
 	private AnalysisResult analyzeBody(final IProgressMonitor monitor, final SystemInstance systemInstance) {
-//		final AnalysisResult analysisResult = ResultUtil.createAnalysisResult("Bus  Load", systemInstance);
-//		analysisResult.setResultType(ResultType.SUCCESS);
-//		analysisResult.setMessage("Bus load analysis of " + systemInstance.getFullName());
+		final AnalysisResult analysisResult = ResultUtil.createAnalysisResult("Mixed Trust Scheduling", systemInstance);
+		analysisResult.setResultType(ResultType.SUCCESS);
+		analysisResult.setMessage("Mixed Trust Scheduling of " + systemInstance.getFullName());
 
 		final SOMIterator soms = new SOMIterator(systemInstance);
 		while (soms.hasNext()) {
 			final SystemOperationMode som = soms.nextSOM();
-//			final Result somResult = ResultUtil.createResult(
-//					Aadl2Util.isPrintableSOMName(som) ? Aadl2Util.getPrintableSOMMembers(som) : "", som,
-//					ResultType.SUCCESS);
-//			analysisResult.getResults().add(somResult);
+			final Result somResult = ResultUtil.createResult(
+					Aadl2Util.isPrintableSOMName(som) ? Aadl2Util.getPrintableSOMMembers(som) : "", som,
+					ResultType.SUCCESS);
+			analysisResult.getResults().add(somResult);
 
-//			scheduleMixedTrustTasks(systemInstance, som);
-//			final BusLoadModel busLoadModel = BusLoadModelBuilder.buildModel(systemInstance, som);
-//			analyzeBusLoadModel(busLoadModel, somResult, monitor);
+			/* Check the Mixed Trust Processor properties for correctness */
+			final Domains domains = new Domains();
+			for (final ComponentInstance processor : systemInstance
+					.getAllComponentInstances(ComponentCategory.PROCESSOR)) {
+				/*
+				 * NB. getAllComponentInstances() claims to be sensitive to the current system operation mode, but
+				 * this isn't true any more. Need to check if the processor is part of the current som.
+				 */
+				if (processor.isActive(som)) {
+					MixedTrustProperties.getMixedTrustProcessor(processor).map(mixedTrustBindings -> {
+						checkMixedTrustBindings(somResult, processor, mixedTrustBindings, domains);
+						return null;
+					});
+				}
+			}
+
+			/* Check the MixedTrustTasks properties for correctness */
+			for (final ComponentInstance system : systemInstance.getAllComponentInstances(ComponentCategory.SYSTEM)) {
+				if (system.isActive(som)) {
+					MixedTrustProperties.getMixedTrustTasks(system).map(listOfTasks -> {
+						for (final MixedTrustTask mixedTrustTask : listOfTasks) {
+							checkMixedTrustTask(somResult, mixedTrustTask, domains);
+						}
+						return null;
+					});
+				}
+			}
+
+			// TODO: Errors (but not warnings) prevent scheduling analysis
+
+			// TODO: Check monitor for being cancelled
+
+			// TODO: Modes and property look up-- check that referenced components exist
+
+//			/* Run scheduling for each mixed trust processor */
+//			for (final ComponentInstance system : systemInstance.getAllComponentInstances(ComponentCategory.SYSTEM)) {
+//				if (system.isActive(som)) {
+//					MixedTrustProperties.getMixedTrustTasks(system).map(listOfTasks -> {
+//						for (final MixedTrustTask mixedTrustTask : listOfTasks) {
+////							scheduleTasks(mixedTrustTask, domains);
+//						}
+//						return null;
+//					});
+//				}
+//			}
 		}
 		monitor.done();
 
-//		return analysisResult;
-
-		// TODO: Return AnalysisResult object
-		return null;
+		return analysisResult;
 	}
-
-//	private void scheduleMixedTrustTasks(final SystemInstance systemInstance, final SystemOperationMode som) {
-//		final Optional<List<MixedTrustTask>> v = MixedTrustProperties.getMixedTrustTasks(systemInstance);
-//		if (v.isPresent()) {
-//			final LayeredTrustExactScheduler scheduler = new LayeredTrustExactScheduler();
-//			for (final MixedTrustTask mtt : v.get()) {
-//			}
-//		} else {
-//			// TODO: Report warning
-//		}
-//	}
 
 	// ======================================================================
 	// === Consistency Checking Methods
 	// ======================================================================
 
-	private void checkMixedTrustBindings(final ComponentInstance processor, final MixedTrustBindings mixedTrustBindings,
+	private void checkMixedTrustBindings(final Result result, final ComponentInstance processor, final MixedTrustBindings mixedTrustBindings,
 			final Domains domains) {
 		final InstanceObject guestOS = mixedTrustBindings.getGuestos().orElse(null);
 		final InstanceObject hyperVisor = mixedTrustBindings.getHypervisor().orElse(null);
 
 		if (guestOS == null) {
-			// error
+			error(result, mixedTrustBindings.toPropertyExpression(processor.eResource().getResourceSet()),
+					"Mixed_Trust_Bindings must specifiy a value for field GuestOS");
 		} else {
 			if (!getProcessorBindings(guestOS).contains(processor)) {
 				// error: not directly bound to processor
+				error(result, mixedTrustBindings.toPropertyExpression(processor.eResource().getResourceSet()),
+						"Virtual processor referenced by field GuestOS is not bound to processor "
+								+ processor.getName());
 			}
 		}
 
 		if (hyperVisor == null) {
-			// error
+			error(result, mixedTrustBindings.toPropertyExpression(processor.eResource().getResourceSet()),
+					"Mixed_Trust_Bindings must specifiy a value for field HyperVisor");
 		} else {
 			if (!getProcessorBindings(hyperVisor).contains(processor)) {
 				// error: not directly bound to processor
+				error(result, mixedTrustBindings.toPropertyExpression(processor.eResource().getResourceSet()),
+						"Virtual processor referenced by field HyperVisor is not bound to processor "
+								+ processor.getName());
 			}
 		}
 
@@ -113,7 +203,8 @@ public final class MixedTrustAnalysis {
 			/* Check that only guestOS and hyperVisor are bound to the processor */
 			for (final ComponentInstance ci : getBoundVirtualProcessors(processor)) {
 				if (ci != guestOS && ci != hyperVisor) {
-					// error: extra things bound to the processor
+					error(result, processor, "Component " + ci.getName()
+							+ " is not a GuestOS or HyperVisor but is bound to processor " + processor.getName());
 				}
 			}
 
@@ -122,47 +213,55 @@ public final class MixedTrustAnalysis {
 		}
 	}
 
-	private void checkMixedTrustTask(final MixedTrustTask mtt, final Domains domains) {
+	private void checkMixedTrustTask(final Result result, final MixedTrustTask mtt, final Domains domains) {
+		final RecordValue diagnosticLocation = mtt
+				.toPropertyExpression(result.getModelElement().eResource().getResourceSet());
 		if (mtt.getPeriod().isEmpty()) {
-			// error
+			error(result, diagnosticLocation,
+					"Mixed_Trust_Task must specify a value for field Period");
 		}
 		if (mtt.getDeadline().isEmpty()) {
-			// error
+			error(result, diagnosticLocation,
+					"Mixed_Trust_Task must specify a value for field Deadline");
 		}
 
 		final InstanceObject guestTask = mtt.getGuesttask().orElse(null);
 		final InstanceObject hyperTask = mtt.getHypertask().orElse(null);
-		final InstanceObject guestOsBinding = checkTask(guestTask, domains::isGuestOS);
-		final InstanceObject hyperVisorBinding = checkTask(hyperTask, domains::isHyperVisor);
+		final InstanceObject guestOsBinding = checkTask(result, diagnosticLocation, guestTask, domains::isGuestOS,
+				"GuestOS");
+		final InstanceObject hyperVisorBinding = checkTask(result, diagnosticLocation, hyperTask, domains::isHyperVisor,
+				"HyperVisor");
 		if (guestOsBinding != null && hyperVisorBinding != null && guestOsBinding != hyperVisorBinding) {
-			// error
+			error(result, diagnosticLocation,
+					"GuestOS and HyperVisor are bound to different processors");
 		}
 	}
 
-	private InstanceObject checkTask(final InstanceObject task,
-			final Function<InstanceObject, Boolean> checkTaskMembership) {
+	private InstanceObject checkTask(final Result result, final EObject where, final InstanceObject task,
+			final Function<InstanceObject, Boolean> checkTaskMembership, String fieldName) {
 		if (task == null) {
-			// error
+			error(result, where, "Mixed_Trust_Task must specify a value for field " + fieldName);
 			return null;
 		} else {
 			if (TimingProperties.getPeriod(task).isPresent()) {
-				// warning
+				warning(result, where, "Referenced thread " + task.getName() + " specifies a Period value");
 			}
 			if (TimingProperties.getDeadline(task).isPresent()) {
-				// warning
+				warning(result, where, "Referenced thread " + task.getName() + " specifies a Deadline value");
 			}
 
 			final List<InstanceObject> boundProcs = getProcessorBindings(task);
 			if (boundProcs.isEmpty()) {
-				// error
+				error(result, where, "Referenced thread " + task.getName() + " is not bound");
 				return null;
 			} else if (boundProcs.size() > 1) {
-				// error
+				error(result, where, "Referenced thread " + task.getName() + " bound to more than one component");
 				return null;
 			} else {
 				final InstanceObject boundTo = boundProcs.get(0);
 				if (!checkTaskMembership.apply(boundTo)) {
-					// error
+					error(result, where,
+							"Referenced thread " + task.getName() + " is not bound to a declared " + fieldName);
 					return null;
 				}
 				return boundTo;
@@ -171,8 +270,18 @@ public final class MixedTrustAnalysis {
 	}
 
 	// ======================================================================
-
+	// == Error reporting methods for the visitor
+	// ==
+	// == XXX: Taken from NewBusLoadAnalysis --- should possibly move to the superclass
 	// ======================================================================
+
+	private static void error(final Result result, final EObject io, final String msg) {
+		result.getDiagnostics().add(ResultUtil.createDiagnostic(msg, io, DiagnosticType.ERROR));
+	}
+
+	private static void warning(final Result result, final EObject io, final String msg) {
+		result.getDiagnostics().add(ResultUtil.createDiagnostic(msg, io, DiagnosticType.WARNING));
+	}
 
 	// ======================================================================
 	// == These should be in InstanceModelUtil, the current bound processor
