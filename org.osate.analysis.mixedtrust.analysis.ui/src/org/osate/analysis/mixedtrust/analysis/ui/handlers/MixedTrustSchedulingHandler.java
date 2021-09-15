49605df2871845bef04bbfbfcd312f23290d4487
Aaron Greenhouse <aaron.greenhouse@gmail.com>
2021-09-15 16:38:42 -0400

    Updated UI handler to write the results to CSV file

--- org.osate.analysis.mixedtrust.analysis.ui/src/org/osate/analysis/mixedtrust/analysis/ui/handlers/MixedTrustSchedulingHandler.java
+++ org.osate.analysis.mixedtrust.analysis.ui/src/org/osate/analysis/mixedtrust/analysis/ui/handlers/MixedTrustSchedulingHandler.java
@@ -1,5 +1,9 @@
 package org.osate.analysis.mixedtrust.analysis.ui.handlers;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
@@ -13,13 +17,17 @@ import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
 import org.osate.aadl2.Element;
 import org.osate.aadl2.instance.SystemInstance;
+import org.osate.aadl2.instance.SystemOperationMode;
+import org.osate.aadl2.modelsupport.Activator;
 import org.osate.aadl2.modelsupport.errorreporting.AnalysisErrorReporterManager;
 import org.osate.aadl2.modelsupport.errorreporting.MarkerAnalysisErrorReporter;
 import org.osate.aadl2.modelsupport.util.AadlUtil;
+import org.osate.aadl2.util.Aadl2Util;
 import org.osate.analysis.mixedtrust.analysis.MixedTrustAnalysis;
 import org.osate.result.AnalysisResult;
 import org.osate.result.Diagnostic;
 import org.osate.result.Result;
+import org.osate.result.util.ResultUtil;
 import org.osate.ui.handlers.AbstractAnalysisHandler;
 
 public final class MixedTrustSchedulingHandler extends AbstractAnalysisHandler {
@@ -74,7 +82,7 @@ public final class MixedTrustSchedulingHandler extends AbstractAnalysisHandler {
 				}
 				generateMarkers(analysisResult, errManager);
 				subMonitor.worked(1);
-//				writeCSVFile(analysisResult, outputFile, subMonitor.split(1));
+				writeCSVFile(analysisResult, outputFile, subMonitor.split(1));
 			} catch (final OperationCanceledException e) {
 				cancelled = true;
 			}
@@ -121,5 +129,108 @@ public final class MixedTrustSchedulingHandler extends AbstractAnalysisHandler {
 				// Do nothing.
 			}
 		});
+	}
+
+	// ============================================================
+	// == XXX: Should move this to the superclass, or otherwise
+	// == abstract it somehow
+	// ============================================================
+
+	// === CSV Output methods ===
+
+	private static void writeCSVFile(final AnalysisResult analysisResult, final IFile outputFile,
+			final IProgressMonitor monitor) {
+		final String csvContent = getCSVasString(analysisResult);
+		final InputStream inputStream = new ByteArrayInputStream(csvContent.getBytes());
+
+		try {
+			if (outputFile.exists()) {
+				outputFile.setContents(inputStream, true, true, monitor);
+			} else {
+				outputFile.create(inputStream, true, monitor);
+			}
+		} catch (final CoreException e) {
+			Activator.logThrowable(e);
+		}
+	}
+
+	private static String getCSVasString(final AnalysisResult analysisResult) {
+		final StringWriter writer = new StringWriter();
+		final PrintWriter pw = new PrintWriter(writer);
+		generateCSVforAnalysis(pw, analysisResult);
+		pw.close();
+		return writer.toString();
+	}
+
+	private static void generateCSVforAnalysis(final PrintWriter pw, final AnalysisResult analysisResult) {
+		pw.println(analysisResult.getMessage());
+		pw.println();
+		pw.println();
+		analysisResult.getResults().forEach(somResult -> generateCSVforSOM(pw, somResult));
+	}
+
+	private static void generateCSVforSOM(final PrintWriter pw, final Result somResult) {
+		if (Aadl2Util.isPrintableSOMName((SystemOperationMode) somResult.getModelElement())) {
+			printItem(pw, "Analysis results in modes " + somResult.getMessage());
+			pw.println();
+		}
+
+		/*
+		 * Output the diagnostics (only at the SOM level)
+		 */
+		if (!somResult.getDiagnostics().isEmpty()) {
+			generateCSVforDiagnostics(pw, somResult.getDiagnostics());
+			pw.println();
+		}
+
+		/*
+		 * Output results for each processor
+		 */
+		somResult.getSubResults().forEach(pr -> generateCSVforProcessor(pw, pr));
+		pw.println();
+	}
+
+	private static void generateCSVforProcessor(final PrintWriter pw, final Result processorResult) {
+		printItem(pw, String.format("Mixed trust tasks on processor %s %s schedulable", processorResult.getMessage(),
+				ResultUtil.getBoolean(processorResult, 0) ? "are" : "are not"));
+		pw.println();
+
+		processorResult.getSubResults().forEach(tr -> generateCSVforTask(pw, tr));
+		pw.println();
+	}
+
+	private static void generateCSVforTask(final PrintWriter pw, final Result taskResult) {
+		printItems(pw, taskResult.getMessage(), ResultUtil.getString(taskResult, 1),
+				ResultUtil.getString(taskResult, 2),
+				String.format("%d microseconds", ResultUtil.getInteger(taskResult, 0)));
+	}
+
+	// ==== Low-level CSV format, this should be abstracted somewhere
+
+	private static void generateCSVforDiagnostics(final PrintWriter pw, final List<Diagnostic> diagnostics) {
+		for (final Diagnostic issue : diagnostics) {
+			printItem(pw, issue.getDiagnosticType().getName() + ": " + issue.getMessage());
+			pw.println();
+		}
+	}
+
+	private static void printItems(final PrintWriter pw, final String item1, final String... items) {
+		printItem(pw, item1);
+		for (final String nextItem : items) {
+			printSeparator(pw);
+			printItem(pw, nextItem);
+		}
+		pw.println();
+	}
+
+	private static void printItem(final PrintWriter pw, final String item) {
+		// TODO: Doesn't handle quotes in the item!
+		pw.print('"');
+		pw.print(item);
+		pw.print('"');
+	}
+
+	private static void printSeparator(final PrintWriter pw) {
+		pw.print(",");
 	}
 }
