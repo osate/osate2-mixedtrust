e714f8a0da98cacfe31af80ffd253f159a547a27
Lutz Wrage <lutz.wrage@gmail.com>
2021-12-10 09:20:49 -0500

    Copy correct repository

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 set -x
 
-TODIR=/var/www/html/p2/ocarina
-rm -fr ${TODIR}
-mkdir -p ${TODIR}
+FROMDIR=org.osate.analysis.mixedtrust.repository/target/repository
+TODIR=/var/www/html/p2/osate2-mixedtrust
 
-cd org.osate.ocarina.repository/target
-cp --recursive repository/* ${TODIR}/.
-
-FROMDIR=org.osate.asap.updatesite/target/repository
-TODIR=/var/www/html/download/osate/experimental/osate2-asap
 rm -fr ${TODIR}
 mkdir -p ${TODIR}
 cp --recursive ${FROMDIR}/* ${TODIR}/.
